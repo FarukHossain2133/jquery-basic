79f05983b056d61a19205fd43376adf0e7e2535d
faruk <fa905432@gmail.com>
2019-10-22 12:02:43 +0600

    Jquery basic part

---
+++ json.js
@@ -0,0 +1,8 @@
+
+var myObj = {
+    people: [
+        {"fname": "Mike", "lname": 'Smit"', "age": 30},
+        {"fname": "John", "lname": 'Doe"', "age": 35},
+        {"fname": "Faruki", "lname": 'Hossain"', "age": 21}
+    ]
+};
